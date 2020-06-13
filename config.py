8c00a0c688f4d386637302d3651211765110f681
Artur Kabirov <arturkabirov16@gmail.com>
2020-06-13 12:01:27 +0600

    find delete comments

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-options='schemma'#schema
-database="insta_scraper"#dbname
-user="postgres"#user
-password="postgres"#password
-host="localhost"#host
-port=5433#port
-path_to_json='za_dzhumabekova\\za_dzhumabekova.json'#path to json file
+options=''#schema
+database=""#dbname
+user=""#user
+password=""#password
+host=""#host
+port=#port
+path_to_json=''#path to json file
 
 
-instagram_target='za_dzhumabekova'#account to scrap
-instagram_user= 'peechka123'#your account
-instagram_password = 'a21b12XY'#your password
 
+instagram_target=''#account to scrap
+instagram_user= ''#your account
+instagram_password = ''#your password
